04359faf8c3b924398f7a2b447276dec1cdbf9d0
K-onrad <g.konrad@outlook.com>
2020-03-12 09:29:38 -0300

    bla

--- .config/polybar/launch.fish
+++ .config/polybar/launch.fish
@@ -4,12 +4,12 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null
+while pgrep -U 1000 -x polybar >/dev/null
 	sleep 1
 end
 
 function launch_bar
-	env MONITOR=$argv[1] polybar $argv[2] &
+        fish -c 'env MONITOR=$argv[1] polybar $argv[2]'
 end
 
 for screen in (xrandr --query | rg -w connected)
